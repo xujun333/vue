a7b6159af8566577f2a041d2f4e75d01d9bab6d1
xujun <1162034699.qq.com>
2018-01-03 20:37:54 +0800

    轮播图2

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -7,6 +7,44 @@
                 <img :src="item.img" alt="">
             </mt-swipe-item>
         </mt-swipe>
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu1.png" alt="">
+                    <div class="mui-media-body">新闻资讯</div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu2.png" alt="">
+                    <div class="mui-media-body">图片分享</div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu3.png" alt="">
+                    <div class="mui-media-body">商品购买</div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu4.png" alt="">
+                    <div class="mui-media-body">留言反馈</div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu5.png" alt="">
+                    <div class="mui-media-body">视频专区</div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <a href="#">
+                    <img src="../../images/menu6.png" alt="">
+                    <div class="mui-media-body">联系我们</div>
+                </a>
+            </li>
+        </ul> 
     </div>
 </template>
 
@@ -58,4 +96,20 @@
             }
         }
     }
+
+
+    .mui-grid-view.mui-grid-9{
+      background-color: #fff;
+      border:0;
+      img {
+        width: 60px;
+        height: 60px;
+      }
+    .mui-media-body{
+        border:0;
+        font-size:13px;
+    }
+
+    }
+
 </style>
